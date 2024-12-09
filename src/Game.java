f50abbb972a3dac7ce07bf2d1cd64a1ca6191bc7
ctechel <carter.techel@menloschool.org>
2024-12-09 13:41:48 -0800

    Finished game

--- src/Game.java
+++ src/Game.java
@@ -20,10 +20,12 @@ public class Game {
     // Constructor
     public Game()
     {
+        // Make a deck for the game
         deck = new Deck(this.ranks, this.suits, this.values);
         deck.shuffle();
         players = new ArrayList<Player>();
         Scanner s1 = new Scanner(System.in);
+        // Get the players in the games name
         System.out.println("What is the name of player 1? Make sure all the names are different.");
         players.add(new Player(s1.nextLine()));
         String player1 = players.get(0).getName();
@@ -44,18 +46,22 @@ public class Game {
             players.get(2).addCard(deck.deal());
             players.get(3).addCard(deck.deal());
         }
+        // Tell each player what their card are
         System.out.println(player1 + "'s hand is: " + players.get(0).getHand());
         System.out.println(player2 + "'s hand is: " + players.get(1).getHand());
         System.out.println(player3 + "'s hand is: " + players.get(2).getHand());
         System.out.println(player4 + "'s hand is: " + players.get(3).getHand());
+        // Asks which player starts the game
         System.out.println("\nWhich player has the 2 of Clubs? (Enter a number 1-4)");
         counter = s1.nextInt() - 1;
+        // Have the first player play the 2 of Clubs
         System.out.println(("It is " + players.get(counter).getName()) + "'s turn. You have to play the 2 of Clubs. What index is it at?");
         idx = s1.nextInt();
         this.max = 2;
         this.handWin = players.get(counter);
         this.points = 0;
         players.get(counter).removeCard(idx);
+        // Have the other 3 players take there turns
         cardWin = new Card("2","Clubs", 2);
         this.suit = "Clubs";
         System.out.println("You played " + players.get(counter).getHand(idx));
@@ -77,6 +83,7 @@ public class Game {
                 "of the card, the first card is 0. This is your hand: " + players.get(counter).getHand());
         idx = s1.nextInt();
         System.out.println("You played " + players.get(counter).getHand(idx));
+        // Find the winner of the hand and give them points
         this.max = checkMax(players.get(counter).getHand(idx), players.get(counter));
         this.points += getPoints(players.get(counter).getHand(idx));
         players.get(counter).removeCard(idx);
@@ -84,44 +91,55 @@ public class Game {
         counter = getStart(handWin);
         System.out.println("The winner of the hand was the " + cardWin);
         round++;
+        // Until all of the rounds are over
         while (!gameOver())
         {
+            // Tell the user whos turn it is and have them choose a card to play
             this.points = 0;
             System.out.println("It is " + this.handWin.getName() + "'s turn. Choose a card to play by the index " +
                     "of the card, the first card is 0. This is your hand: " + players.get(counter).getHand());
             idx = s1.nextInt();
             System.out.println("You played " + players.get(counter).getHand(idx));
+            // Set the winning card to the card just played
             cardWin = new Card(players.get(counter).getHand(idx).getRank(), players.get(counter).getHand(idx).getSuit(), players.get(counter).getHand(idx).getValue());
             this.max = cardWin.getValue();
             this.suit = cardWin.getSuit();
             this.points += getPoints(players.get(counter).getHand(idx));
             players.get(counter).removeCard(idx);
+            // Ask the second player for there card
             System.out.println("It is " + players.get(getTurn()).getName() + "'s turn. Choose a card to play by the index " +
                     " of the card, the first card is 0. This is your hand: " + players.get(counter).getHand());
             idx = s1.nextInt();
             System.out.println("You played " + players.get(counter).getHand(idx));
+            // Adjust the winning card accordingly
             this.max = checkMax(players.get(counter).getHand(idx), players.get(counter));
             this.points += getPoints(players.get(counter).getHand(idx));
             players.get(counter).removeCard(idx);
+            // Ask the third player for there card
             System.out.println("It is " + players.get(getTurn()).getName() + "'s turn. Choose a card to play by the index " +
                     " of the card, the first card is 0. This is your hand: " + players.get(counter).getHand());
             idx = s1.nextInt();
             System.out.println("You played " + players.get(counter).getHand(idx));
+            // Adjust the winning card accordingly
             this.max = checkMax(players.get(counter).getHand(idx), players.get(counter));
             this.points += getPoints(players.get(counter).getHand(idx));
             players.get(counter).removeCard(idx);
+            // Ask the fourth player for there card
             System.out.println("It is " + players.get(getTurn()).getName() + "'s turn. Choose a card to play by the index " +
                     " of the card, the first card is 0. This is your hand: " + players.get(counter).getHand());
             idx = s1.nextInt();
             System.out.println("You played " + players.get(counter).getHand(idx));
+            // Adjust the winning card accordingly
             this.max = checkMax(players.get(counter).getHand(idx), players.get(counter));
             this.points += getPoints(players.get(counter).getHand(idx));
             players.get(counter).removeCard(idx);
             this.handWin.addPoints(points);
             counter = getStart(handWin);
+            // Tell the players who won the hand
             System.out.println("The winner of the hand was the " + cardWin);
             round++;
         }
+        // Print out the winning scores and if someone "shot the moon" give everyone else 26 points
         for (int i = 0; i < 4; i++)
         {
             if (players.get(i).getPoints() == 26)
@@ -208,8 +226,8 @@ public class Game {
         }
         return false;
     }
-    // Deals each player an amount of cards
 
+    // Print instructions
     private static void printInstructions()
     {
         System.out.println("Welcome to Hearts!!! " +
@@ -224,6 +242,7 @@ public class Game {
                 "points in the round you get 0 points and everyone else gets 26. \n");
     }
 
+    // Create the game in the main function
     public static void main(String[] args)
     {
         printInstructions();
